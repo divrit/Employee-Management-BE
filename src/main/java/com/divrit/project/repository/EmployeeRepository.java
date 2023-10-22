ea028d238520c49ffbe647584322f420b4522c65
divrit <divrit97@gmail.com>
2023-10-22 14:58:16 -0700

    BE for Employee management

---
+++ src/main/java/com/divrit/project/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.divrit.project.repository;
+
+import com.divrit.project.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
